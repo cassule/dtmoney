ae4f0fc62a4676f36b130a7fb8292e3711f54f28
Josué Francisco <josuefrancisco2017@outlook.pt>
2021-12-24 22:05:59 +0100

    contextos e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -11,6 +13,7 @@ Modal.setAppElement('#root');
 
 function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalIOpen] = useState(false);
+  
 
   function handleOpenNewTransactionModal(){
     setIsNewTransactionModalIOpen(true);
@@ -21,7 +24,8 @@ function App() {
   }
 
   return (
-    <>
+
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -32,7 +36,7 @@ function App() {
       />
 
       <GlobalStyle />
-    </>
+    </ TransactionsProvider>
     
   );
 }
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useTransactions } from '../../hooks/useTransactions';
+
 import { Container } from "./styles";
 
-interface TransactionsProps {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-}
-
 export function TransactionsTable(){
-  const [transactions,setTransactions] = useState<TransactionsProps[]>([]);
-  
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data.transactions));
-  }, [])
+  const { transactions } = useTransactions();
 
   return (
     <Container>
@@ -35,16 +21,16 @@ export function TransactionsTable(){
               <td>{transaction.title}</td>
               <td className={transaction.type}>
                 {
-                  new Intl.NumberFormat('pt-BR', {
+                  new Intl.NumberFormat('pt-AO', {
                     style: 'currency',
-                    currency: 'BRL'
+                    currency: 'AOA'
                   }).format(transaction.amount)
                 }
               </td>
               <td>{transaction.category}</td>
               <td>
                 {
-                  new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))
+                  new Intl.DateTimeFormat('pt-AO').format(new Date(transaction.createdAt))
                 }
               </td>
             </tr>  
